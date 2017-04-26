4d73b7551874ab3281b2966b057756b45fb75fd6
subhanibabu <subhani_anchula@epam.com>
2017-04-27 00:10:16 +0530

    upadted with review comments.

--- getmiddlecharacter.js
+++ getmiddlecharacter.js
@@ -2,21 +2,13 @@ function getMiddle(Word)
 {
   var length = Word.length,output;
 
-if(length % 2 == 0)
-{
-output = Word[(length/2)-1] + Word[(length/2)];
-}
-else
-{
-  if(length==1)
-  {
-  output = Word;
-  }
+  if(length % 2 == 0)
+    {
+    output = Word.slice((length/2)-1,(length/2)+1);
+    }
   else
-  {
-  output = Word[Math.floor((length/2))];
-  }
-}
-
+    {
+      output = Word.charAt((length/2));
+    }
 return output ;
 }
