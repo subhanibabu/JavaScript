1384c109ede3540b14de55a7cd9bb8330c4cf960
subhanibabu <subhani_anchula@epam.com>
2017-05-02 18:57:11 +0530

    Update vacation.js

--- vacation.js
+++ vacation.js
@@ -1,10 +1,10 @@
 function rentalCarCost(days) {
-    var Discount = 0,
-        Rate = days * 40;
-    if (days >= 7) {
+    var Rate = days * 40;
+    if (days >= 7)
         Discount = 50;
-    } else if (days >= 3) {
+    else if (days >= 3)
         Discount = 20;
-    }
+    else
+        Discount = 0;
     return Rate - Discount;
 }
