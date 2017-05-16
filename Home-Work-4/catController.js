e0a60e17eb825234c72b83483943969ce9258f87
subhanibabu <subhani_anchula@epam.com>
2017-05-17 01:05:30 +0530

    Update catController.js

--- Home-Work-4/catController.js
+++ Home-Work-4/catController.js
@@ -1,8 +1,20 @@
 angular.module('catClick', [])
-  .controller('catCickController', ['$scope', function($scope) {
-    var vm = this;
-    vm.count = 0;      
-    vm.myFunc = function() {
-      vm.count++;
-    };
-  }]);
+    .controller('catCickController', function($scope) {
+        var vm = this;
+        vm.count = 0;
+        vm.clickCount1 = 0;
+        vm.clickCount2 = 0;
+
+        vm.myFunc = function() {
+            vm.count++;
+        };
+
+        vm.catmyFunc1 = function() {
+            vm.clickCount1++;
+        };
+
+        vm.catmyFunc2 = function() {
+            vm.clickCount2++;
+        };
+
+    });
