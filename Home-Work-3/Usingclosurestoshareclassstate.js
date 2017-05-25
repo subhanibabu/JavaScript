df0668a340900de699ac58f2c4bbe0e2eecdba5a
subhanibabu <subhani_anchula@epam.com>
2017-05-25 10:58:36 +0530

    Update Usingclosurestoshareclassstate.js

--- Home-Work-3/Usingclosurestoshareclassstate.js
+++ Home-Work-3/Usingclosurestoshareclassstate.js
@@ -1,31 +1,26 @@
-var sum = 0, count=[];
- function Cat(name, weight1) { 
+var Cat = (function() {
+    var totalWeight = 0,
+        count = 0;
 
-if (typeof name == 'undefined' || typeof weight1 == 'undefined') {
-         throw new Error('Cats not instantiated with name, weight: ' + name + ', ' + weight);
-     }     
-  this.name = name;
-  this._weight = weight1;
-  sum += this._weight;
-  count.push(this);
- };
-
-Object.defineProperty(Cat.prototype, "weight", {
-     get: function () {     
-         return this._weight;
-     },
-     set: function (wight) {
-         this._weight = wight;
-         sum += this._weight;         
-         },
-        
- });
-
- Object.defineProperty(Cat, "averageWeight", {
-     get: function () {
-         return function () {
-         console.log(count);
-             return count ? sum / count.length : 0;
-         };
-     }
- });
+    function Cat(name, weight) {
+        if (typeof name == 'undefined' || typeof weight == 'undefined') {
+            throw new Error('Cats not instantiated with name, weight: ' + name + ', ' + weight);
+        }
+        this.name = name;
+        Object.defineProperty(this, 'weight', {
+            get: function() {
+                return weight;
+            },
+            set: function(value) {
+                totalWeight -= weight - value;
+                weight = value;
+            }
+        });
+        totalWeight += weight;
+        count += 1;
+    }
+    Cat.averageWeight = function() {
+        return totalWeight / count;
+    };
+    return Cat;
+}());
