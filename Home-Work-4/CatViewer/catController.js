95b0d241543926754e09cd2d3723bfdc9d0bd361
subhanibabu <subhani_anchula@epam.com>
2017-06-03 01:09:53 +0530

    Add files via upload

---
+++ Home-Work-4/CatViewer/catController.js
@@ -0,0 +1,41 @@
+var catViewer = angular.module('catViewer', ['catViewerService']);
+    catViewer.controller('catCickController', function($scope, catService) {
+        var vm = this;
+        vm.votes = 0;
+        
+        vm.cats = catService.GetCats();
+    
+        vm.Allcats = [];
+        vm.Allcats = vm.cats;
+
+        vm.selectedCat = function() {
+            vm.votes = 0;            
+        }
+        vm.changeVote = function(flag) {
+            if (flag == 'up') {
+                vm.votes++;
+            } else {
+                vm.votes--;
+            }
+        }
+        vm.search = function() {
+            if (vm.searchText == '') {
+                vm.Allcats = vm.cats;
+            } else {
+                vm.Allcats = vm.cats.filter(function(cat) {
+                    return vm.searchText == cat.name;
+                });
+                vm.Allcats[0].catViewed=true;
+            }
+        }
+        vm.addCat = function(){
+            catService.AddCat(vm.name, vm.url);
+        }
+        vm.resetAll = function() {
+            vm.Allcats.pop({ name: vm.name, url: vm.url });
+            vm.name = '';
+            vm.url = '';
+            vm.searchText = '';
+        }
+    
+    });
