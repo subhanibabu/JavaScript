669e065296e8c239577f0ac02cb94749bcfcd88e
subhanibabu <subhani_anchula@epam.com>
2017-06-04 23:18:27 +0530

    Add files via upload

--- Home-Work-4/CatViewer/catController.js
+++ Home-Work-4/CatViewer/catController.js
@@ -1,13 +1,13 @@
-var catViewer = angular.module('catViewer', ['catViewerService']);
+var catViewer = angular.module('routeApp', ['catViewerService']);
     catViewer.controller('catCickController', function($scope, catService) {
         var vm = this;
         vm.votes = 0;
+        vm.Allcats = [];
         
         vm.cats = catService.GetCats();
-    
-        vm.Allcats = [];
+        
         vm.Allcats = vm.cats;
-
+       
         vm.selectedCat = function() {
             vm.votes = 0;            
         }
@@ -18,24 +18,13 @@ var catViewer = angular.module('catViewer', ['catViewerService']);
                 vm.votes--;
             }
         }
-        vm.search = function() {
-            if (vm.searchText == '') {
-                vm.Allcats = vm.cats;
-            } else {
-                vm.Allcats = vm.cats.filter(function(cat) {
-                    return vm.searchText == cat.name;
-                });
-                vm.Allcats[0].catViewed=true;
-            }
-        }
+
         vm.addCat = function(){
             catService.AddCat(vm.name, vm.url);
         }
+        
         vm.resetAll = function() {
             vm.Allcats.pop({ name: vm.name, url: vm.url });
-            vm.name = '';
-            vm.url = '';
-            vm.searchText = '';
         }
     
     });
---
+++ Home-Work-4/CatViewer/catSource.js
@@ -0,0 +1,27 @@
+var cats = [];        
+        cats = [{
+            name: 'India',
+            url: 'http://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&amp;h=426',
+            catViewed:false
+
+        }, {
+            name: 'Australia',
+            url: 'http://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454',
+            catViewed:false
+
+        }, {
+            name: 'South Africa',
+            url: 'http://www.businessinsider.com/image/4f3433986bb3f7b67a00003c/cute-cat.jpg',
+            catViewed:false
+
+        }, {
+            name: 'New Zealand',
+            url: 'http://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&amp;h=426',
+            catViewed:false
+
+        }, {
+            name: 'England',
+            url: 'http://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+            catViewed:false
+
+        }];
